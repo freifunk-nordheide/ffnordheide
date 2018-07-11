10550d28c369670fb43791dfba13684db35f1a0c
rubo77 <github@r.z11.de>
2018-07-11 09:32:06 +0200

    anpassung an 2018.1

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Site configuration Freifunk-Nordheide e.V.
     git clone https://github.com/freifunk-nordheide/ffnordheide
     cp -a ffnordheide/ffnh-site gluon/site
     cd gluon
-    git checkout v2017.1.4
+    git checkout v2018.1
     
     # for roamguide:
     git config http.sslVerify "false"
--- ffndh-site/site.mk
+++ ffndh-site/site.mk
@@ -43,8 +43,8 @@ GLUON_SITE_PACKAGES += \
 	
 # Always call `make` from the command line with the desired release version!
 # otherwise this is generated:
-#	DEFAULT_GLUON_RELEASE := 2017.1.4~lede$(shell date '+%y%m%d%H%M')
-	DEFAULT_GLUON_RELEASE := 2018.1
+DEFAULT_GLUON_RELEASE := 2018.1~exp$(shell date '+%y%m%d%H%M')
+#	DEFAULT_GLUON_RELEASE := 2018.1
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
